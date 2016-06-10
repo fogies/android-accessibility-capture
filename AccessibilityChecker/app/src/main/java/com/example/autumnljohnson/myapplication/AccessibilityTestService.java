cabf2b93a5470b9c9487efe3a5131b1c723613f1
Steve <stevezhanggeek@43.7.28.172.dynr.cs.washington.edu>
2016-06-10 15:07:55 -0700

    Request using adb broadcast intent

--- AccessibilityChecker/app/src/main/java/com/example/autumnljohnson/myapplication/AccessibilityTestService.java
+++ AccessibilityChecker/app/src/main/java/com/example/autumnljohnson/myapplication/AccessibilityTestService.java
@@ -40,15 +40,17 @@ public class AccessibilityTestService extends AccessibilityService {
     public String packageName = "";
     private static final String TAG = "AccessibilityService";
     private static AccessibilityTestData data;
-    private BroadcastReceiver yourReceiver;
-    private static final String ACTION="xiaoyiz.triggerIntent";
-
+    private BroadcastReceiver receiver_set_package;
+    private BroadcastReceiver receiver_pull_current_tree;
+    private BroadcastReceiver receiver_pull_current_errors;
+    /*
     @Override
     public void onStart(Intent intent, int startId) {
         super.onStart(intent, startId);
         Bundle extras = intent.getExtras();
         packageName = (String) extras.get("packageName");
     }
+    */
 
     @Override
     protected void onServiceConnected() {
@@ -64,31 +66,83 @@ public class AccessibilityTestService extends AccessibilityService {
     @Override
     public void onCreate() {
         super.onCreate();
-        Log.i("Xiaoyi", "Wow");
-        final IntentFilter theFilter = new IntentFilter();
-        theFilter.addAction(ACTION);
-        this.yourReceiver = new BroadcastReceiver() {
+        final IntentFilter filter_set_package = new IntentFilter();
+        filter_set_package.addAction("xiaoyiz.setPackage");
+        receiver_set_package = new BroadcastReceiver() {
             @Override
             public void onReceive(Context context, Intent intent) {
-                String output = intent.getStringExtra("actionName");
-                Log.i("Xiaoyi", output);
+                packageName = intent.getStringExtra("packageName");
+                Log.i(TAG, "Set Package:" + packageName);
             }
         };
-        // Registers the receiver so that your service will listen for
-        // broadcasts
-        this.registerReceiver(this.yourReceiver, theFilter);
+        this.registerReceiver(receiver_set_package, filter_set_package);
+
+        final IntentFilter filter_pull_current_tree = new IntentFilter();
+        filter_pull_current_tree.addAction("xiaoyiz.pullCurrentTree");
+        receiver_pull_current_tree = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                Log.i(TAG, "Pull Current Tree:");
+                pullCurrentTree();
+            }
+        };
+        this.registerReceiver(receiver_pull_current_tree, filter_pull_current_tree);
+
+        final IntentFilter filter_pull_current_errors = new IntentFilter();
+        filter_pull_current_errors.addAction("xiaoyiz.pullCurrentErrors");
+        receiver_pull_current_errors = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                Log.i(TAG, "Pull Current Errors:");
+                pullCurrentErrors();
+                pullAllErrors();
+            }
+        };
+        this.registerReceiver(receiver_pull_current_errors, filter_pull_current_errors);
+    }
+
+    public void pullCurrentTree() {
+        AccessibilityNodeInfo node = getRootInActiveWindow();
+        if (node == null) return;
+        printAllNodes(node, null);
+    }
+
+    public void pullCurrentErrors() {
+        AccessibilityNodeInfo node = getRootInActiveWindow();
+
+        int numNodes = AccessibilityNodeInfoUtils.searchAllFromBfs(getApplicationContext(),
+                new AccessibilityNodeInfoCompat(node), WIDE_OPEN_FILTER).size();
+        Set<AccessibilityInfoHierarchyCheck> checks =
+                AccessibilityCheckPreset.getInfoChecksForPreset(AccessibilityCheckPreset.VERSION_2_0_CHECKS);
+
+        // 6 total checks (one is Warnings, other are Errors)
+        for (AccessibilityInfoHierarchyCheck check : checks) {
+            try {
+                List<AccessibilityInfoCheckResult> checkResults =
+                        check.runCheckOnInfoHierarchy(node, getApplicationContext());
+                data.storeCheckResults(packageName, checkResults, getCheckType(check), numNodes);
+                Log.i(TAG, "CHECKER_RESULT -> " + data.resultsToJSON());
+                data = new AccessibilityTestData();
+            } catch (Exception ex) { // Probably a DuplicateClickableBounds error
+                Log.d(TAG, ex.getMessage());
+            }
+        }
+    }
+
+    public void pullAllErrors() {
+        Log.i(TAG, getApplicationContext().getExternalFilesDir(null).toString());
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        // Do not forget to unregister the receiver!!!
-        this.unregisterReceiver(this.yourReceiver);
+        this.unregisterReceiver(receiver_set_package);
+        this.unregisterReceiver(receiver_pull_current_tree);
+        this.unregisterReceiver(receiver_pull_current_errors);
     }
 
     @Override
     public boolean onUnbind(Intent intent) {
-        //data.printResults();
         Log.i(TAG, "CHECKER_RESULT -> " + data.resultsToJSON());
         return super.onUnbind(intent);
     }
@@ -98,44 +152,44 @@ public class AccessibilityTestService extends AccessibilityService {
         data.printResults();
     }
 
-
     @Override
     public void onAccessibilityEvent(AccessibilityEvent e) {
 
-        if (e.getPackageName() == null) {
-            return;
-        }
-        Log.d(TAG, "on accessibility event");
+        if (e.getPackageName() == null) return;
+
+        /*
+        Log.i(TAG, "on accessibility event");
         // node that can be used to traverse the window content, represented as a tree of such objects.
         AccessibilityNodeInfo node = getRootInActiveWindow();
+        if (node == null) return;
 
-        if (node != null) {
-            // get the number of nodes in the hierarchy (number of nodes that tests will be run on)
-            int numNodes = AccessibilityNodeInfoUtils.searchAllFromBfs(getApplicationContext(),
-                    new AccessibilityNodeInfoCompat(node), WIDE_OPEN_FILTER).size();
+        // get the number of nodes in the hierarchy (number of nodes that tests will be run on)
+        int numNodes = AccessibilityNodeInfoUtils.searchAllFromBfs(getApplicationContext(),
+                new AccessibilityNodeInfoCompat(node), WIDE_OPEN_FILTER).size();
 
-            if (e.getPackageName().equals(packageName)) {
-                String name = e.getPackageName()+", screen "+Integer.toString(e.getWindowId());
-                Log.i(TAG, "--------------");
-                Log.i(TAG, "Name -> " + name);
+        if (e.getPackageName().equals(packageName)) {
+            String name = e.getPackageName()+", screen "+Integer.toString(e.getWindowId());
+            Log.i(TAG, "--------------");
+            Log.i(TAG, "Name -> " + name);
 
-                printAllNodes(getRootInActiveWindow(), null);
+            printAllNodes(getRootInActiveWindow(), null);
 
-                Set<AccessibilityInfoHierarchyCheck> checks = AccessibilityCheckPreset.getInfoChecksForPreset(AccessibilityCheckPreset.VERSION_2_0_CHECKS);
+            Set<AccessibilityInfoHierarchyCheck> checks = AccessibilityCheckPreset.getInfoChecksForPreset(AccessibilityCheckPreset.VERSION_2_0_CHECKS);
 
-                // 6 total checks (one is Warnings, other are Errors)
-                for (AccessibilityInfoHierarchyCheck check : checks) {
-                    try {
-                        List<AccessibilityInfoCheckResult> checkResults =
-                                check.runCheckOnInfoHierarchy(node, getApplicationContext());
-                        data.storeCheckResults(e.getPackageName(), checkResults, getCheckType(check), numNodes);
-                        Log.i(TAG, "CHECKER_RESULT -> " + data.resultsToJSON());
-                        data = new AccessibilityTestData();
-                    } catch (Exception ex) { // Probably a DuplicateClickableBounds error
-                        Log.d(TAG, ex.getMessage());
-                    }
+            // 6 total checks (one is Warnings, other are Errors)
+            for (AccessibilityInfoHierarchyCheck check : checks) {
+                try {
+                    List<AccessibilityInfoCheckResult> checkResults =
+                            check.runCheckOnInfoHierarchy(node, getApplicationContext());
+                    data.storeCheckResults(e.getPackageName(), checkResults, getCheckType(check), numNodes);
+                    Log.i(TAG, "CHECKER_RESULT -> " + data.resultsToJSON());
+                    data = new AccessibilityTestData();
+                } catch (Exception ex) { // Probably a DuplicateClickableBounds error
+                    Log.d(TAG, ex.getMessage());
                 }
             }
+        }
+        */
 
             /*
             Bundle bundle = new Bundle();
@@ -146,7 +200,6 @@ public class AccessibilityTestService extends AccessibilityService {
                 Log.d(TAG, "warning " + e.getPackageName() + ":" + warnings.get(0).getMessage().toString());
             }
             */
-        }
     }
 
     private static final NodeFilter WIDE_OPEN_FILTER = new NodeFilter() {
@@ -181,15 +234,18 @@ public class AccessibilityTestService extends AccessibilityService {
     private void printAllNodes(AccessibilityNodeInfo info, AccessibilityNodeInfo parent) {
         if (info == null) return;
         if (info.getChildCount() == 0) {
-            Log.i(TAG, "TREE_RESULT -> " + "Current Node = " + info.toString());
+            Log.i(TAG, "TREE_RESULT -> " + "Leaf Node = " + info.toString());
             Rect bounds = new Rect();
             info.getBoundsInScreen(bounds);
             Log.i(TAG, "Node Center Coordinate -> x = " + bounds.centerX() + ", y = " + bounds.centerY());
         } else {
+            Log.i(TAG, "TREE_RESULT -> " + "Non-leaf Node = " + info.toString());
+            Rect bounds = new Rect();
+            info.getBoundsInScreen(bounds);
+            Log.i(TAG, "Node Center Coordinate -> x = " + bounds.centerX() + ", y = " + bounds.centerY());
             for (int i = 0; i < info.getChildCount(); i++) {
                 printAllNodes(info.getChild(i), info);
             }
         }
     }
-
 }
--- AccessibilityChecker/app/src/main/java/com/example/autumnljohnson/myapplication/MainActivity.java
+++ AccessibilityChecker/app/src/main/java/com/example/autumnljohnson/myapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.autumnljohnson.myapplication;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.Settings;
@@ -8,26 +9,14 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final Intent intentToAccessibility = new Intent(this, AccessibilityTestService.class); // this was me
-
-        this.findViewById(R.id.setPackageButton).setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View arg0) {
-                stopService(intentToAccessibility); // this was me
-                EditText mEdit = (EditText)findViewById(R.id.packageName);
-                String packageName = mEdit.getText().toString();
-                intentToAccessibility.putExtra("packageName", packageName);
-                startService(intentToAccessibility); // this was me
-            }
-        });
 
         this.findViewById(R.id.accessibilitySettingButton).setOnClickListener(new View.OnClickListener() {
 
@@ -36,28 +25,5 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS));
             }
         });
-
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
